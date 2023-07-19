d5ad3aa0c3c38c8aea169f0b2937c3e260082909
Taimoor Ali <taimoorali083@gmail.com>
2023-07-19 11:41:00 +0500

    fetching blogpost using /getblog Endpoint

--- src/pages/blogpost/[slug].js
+++ src/pages/blogpost/[slug].js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React , {useState , useEffect} from 'react';
 import { useRouter } from 'next/router';
 import styles from '../../styles/Blogpost.module.css'
 
 //Find the file corresponding to the slug
 //populate them inside the page
 const Slug = () => {
+  const [blog, setBlog] = useState();
+
   const router = useRouter();
-  const { slug } = (router.query)
+
+  useEffect(() => {
+    if(!router.isReady)return;
+    const { slug } = (router.query)
+   fetch(`http://localhost:3000/api/getblogs?slug=${slug}`).then((d)=>{
+    return d.json()
+   }).then((parsedata)=>{
+    setBlog(parsedata);
+   })
+  }, [router.isReady]);
+
+  
   return (
     <div className={styles.container}>
       <main className={styles.main} >
 
-      <h1>The title of the page is : {slug} </h1>
-      <p>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem perferendis aperiam, similique id quibusdam sequi impedit rerum cumque quam pariatur, vero consequuntur, asperiores dolorum autem. Atque, alias et. Est architecto rem, fugiat quidem sunt error libero corrupti mollitia, voluptatum reiciendis iusto nobis, eum facilis illo! consectetur adipisicing elit. Dicta, tempora quod eaque adipisci quam temporibus ad facilis necessitatibus assumenda quisquam consectetur officia rerum.</p>
-      </main>
+      <h1>{blog && blog.title} </h1>
+      <p>{blog && blog.content}</p> </main>
     </div>
   );
 }
